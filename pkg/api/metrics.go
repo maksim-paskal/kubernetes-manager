8fd980af4407572b58f01d6661dcfae7fbc36c96
Maksim Paskal <paskal.maksim@gmail.com>
2022-03-09 14:59:47 +0200

    bump kubernetes to v0.21.10 (#62)
    Signed-off-by: Maksim Paskal <paskal.maksim@gmail.com>

--- pkg/api/metrics.go
+++ pkg/api/metrics.go
@@ -13,6 +13,7 @@ limitations under the License.
 package api
 
 import (
+	"context"
 	"net/url"
 	"time"
 
@@ -23,7 +24,7 @@ type requestResult struct {
 	Cluster string
 }
 
-func (r *requestResult) Increment(code string, method string, host string) {
+func (r *requestResult) Increment(ctx context.Context, code string, method string, host string) {
 	metrics.KubernetesAPIRequest.WithLabelValues(host, code).Inc()
 }
 
@@ -31,6 +32,6 @@ type requestLatency struct {
 	Cluster string
 }
 
-func (r *requestLatency) Observe(verb string, u url.URL, latency time.Duration) {
+func (r *requestLatency) Observe(ctx context.Context, verb string, u url.URL, latency time.Duration) {
 	metrics.KubernetesAPIRequestDuration.WithLabelValues(u.Host).Observe(latency.Seconds())
 }
